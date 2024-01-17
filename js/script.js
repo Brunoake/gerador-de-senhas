5a83aea863ac20652eda8523fc3645c20c0d0563
Brunoake <126792752+Brunoake@users.noreply.github.com>
2024-01-16 23:40:45 -0300

    .

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// seleção de elementos
+const generatePasswordButton = document.querySelector("#generate-password")
+
+const generatedPasswordElement = document.querySelector("#generated-password")
+
+// Funções
+const getLetterLowerCase = () => {
+
+    return (String.fromCharCode (Math.floor(Math.random() * 26) + 97));
+
+}
+
+const getLetterUpperCase = () => {
+
+    return (String.fromCharCode (Math.floor(Math.random() * 26) + 65));
+
+}
+
+const getNumber = () => {
+    return Math.floor( Math.random() * 10).toString();
+}
+
+const getSymbol = () => {
+    const symbols = "(){}[]=<>/,.!@#$%&*+-"
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getSymbol());
+
+// Eventos
+generatePasswordButton.addEventListener("click", () => {
+
+   
+
+})
